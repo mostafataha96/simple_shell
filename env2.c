23440c6e7491d5f1b274ec233d27e0b677f60bdd
mostafataha96 <133124977+mostafataha96@users.noreply.github.com>
2023-08-26 05:47:08 +0300

    Update env2.c

--- env2.c
+++ env2.c
@@ -53,7 +53,7 @@ int _unsetenv(info_t *info, char *var)
  * _setenv - Initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ *        constant function prototype
  * @var: the string env var property
  * @value: the string env var value
  *  Return: Always 0
