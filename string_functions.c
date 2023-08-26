7ad40ee0b4296b54b8c3736190b3a763c1b24078
mostafataha96 <133124977+mostafataha96@users.noreply.github.com>
2023-08-26 05:42:44 +0300

    Update string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -3,7 +3,6 @@
 /**
  * _strlen - returns the length of a string
  * @s: the string whose length to check
- *
  * Return: integer length of string
  */
 int _strlen(char *s)
@@ -22,7 +21,6 @@ int _strlen(char *s)
  * _strcmp - performs lexicogarphic comparison of two strangs.
  * @s1: the first strang
  * @s2: the second strang
- *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
@@ -44,7 +42,6 @@ int _strcmp(char *s1, char *s2)
  * starts_with - checks if needle starts with haystack
  * @haystack: string to search
  * @needle: the substring to find
- *
  * Return: address of next char of haystack or NULL
  */
 char *starts_with(const char *haystack, const char *needle)
@@ -59,7 +56,6 @@ char *starts_with(const char *haystack, const char *needle)
  * _strcat - concatenates two strings
  * @dest: the destination buffer
  * @src: the source buffer
- *
  * Return: pointer to destination buffer
  */
 char *_strcat(char *dest, char *src)
