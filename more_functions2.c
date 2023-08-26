d52d99a0f78296a1bc8797f638b7e0a326361ee3
mostafataha96 <133124977+mostafataha96@users.noreply.github.com>
2023-08-26 05:23:29 +0300

    more_functions2.c

--- more_functions2.c
+++ more_functions2.c
@@ -8,17 +8,17 @@
  */
 int _erratoi(char *s)
 {
-	int ii = 0;
+	int i = 0;
 	unsigned long int result = 0;
 
 	if (*s == '+')
 		s++;  /* TODO: why does this make main return 255? */
-	for (ii = 0;  s[ii] != '\0'; ii++)
+	for (i = 0;  s[i] != '\0'; i++)
 	{
-		if (s[ii] >= '0' && s[ii] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result *= 10;
-			result += (s[ii] - '0');
+			result += (s[i] - '0');
 			if (result > INT_MAX)
 				return (-1);
 		}
@@ -56,8 +56,8 @@ void print_error(info_t *info, char *estr)
 int print_d(int input, int fd)
 {
 	int (*__putchar)(char) = _putchar;
-	int ii, countt = 0;
-	unsigned int _abs_, currentt;
+	int i, count = 0;
+	unsigned int _abs_, current;
 
 	if (fd == STDERR_FILENO)
 		__putchar = _eputchar;
@@ -65,24 +65,24 @@ int print_d(int input, int fd)
 	{
 		_abs_ = -input;
 		__putchar('-');
-		countt++;
+		count++;
 	}
 	else
 		_abs_ = input;
-	currentt = _abs_;
-	for (ii = 1000000000; ii > 1; ii /= 10)
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
 	{
-		if (_abs_ / ii)
+		if (_abs_ / i)
 		{
-			__putchar('0' + currentt / ii);
-			countt++;
+			__putchar('0' + current / i);
+			count++;
 		}
-		currentt %= ii;
+		current %= i;
 	}
-	__putchar('0' + currentt);
-	countt++;
+	__putchar('0' + current);
+	count++;
 
-	return (countt);
+	return (count);
 }
 
 /**
@@ -90,20 +90,21 @@ int print_d(int input, int fd)
  * @num: number
  * @base: base
  * @flags: argument flags
+ *
  * Return: string
  */
 char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
 	static char buffer[50];
-	char signn = 0;
+	char sign = 0;
 	char *ptr;
-	unsigned long nn = num;
+	unsigned long n = num;
 
 	if (!(flags & CONVERT_UNSIGNED) && num < 0)
 	{
-		nn = -num;
-		signn = '-';
+		n = -num;
+		sign = '-';
 
 	}
 	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
@@ -111,28 +112,29 @@ char *convert_number(long int num, int base, int flags)
 	*ptr = '\0';
 
 	do	{
-		*--ptr = array[nn % base];
-		nn /= base;
-	} while (nn != 0);
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
 
-	if (signn)
-		*--ptr = signn;
+	if (sign)
+		*--ptr = sign;
 	return (ptr);
 }
 
 /**
  * remove_comments - function replaces first instance of '#' with '\0'
  * @buf: address of the string to modify
+ *
  * Return: Always 0;
  */
 void remove_comments(char *buf)
 {
-	int ii;
+	int i;
 
-	for (ii = 0; buf[ii] != '\0'; ii++)
-		if (buf[ii] == '#' && (!ii || buf[ii - 1] == ' '))
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
 		{
-			buf[ii] = '\0';
+			buf[i] = '\0';
 			break;
 		}
 }
