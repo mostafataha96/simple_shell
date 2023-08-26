a15882350790cad14d8e73e2622ff3076cf3632e
mostafataha96 <133124977+mostafataha96@users.noreply.github.com>
2023-08-26 05:21:55 +0300

    main.c

--- main.c
+++ main.c
@@ -4,22 +4,23 @@
  * main - entry point
  * @ac: arg count
  * @av: arg vector
+ *
  * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int fdd = 2;
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r" (fdd)
-		: "r" (fdd));
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
-		fdd = open(av[1], O_RDONLY);
-		if (fdd == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
